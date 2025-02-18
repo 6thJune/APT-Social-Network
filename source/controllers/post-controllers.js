242d444a87b1a89c3409dfcb39180999e8e41959
Nguyễn Thanh Sang <sangnt6604@gmail.com>
2025-02-18 23:20:06 +0700

    Cloudinary

--- source/controllers/post-controllers.js
+++ source/controllers/post-controllers.js
@@ -2,21 +2,7 @@ const express = require('express');
 const postController = express.Router();
 const User = require('../models/user');
 const Post = require('../models/post');
-const path = require('path');
-const fs = require('fs');
-const util = require('util');
-const unlinkAsync = util.promisify(fs.unlink);
-const multer = require('multer');
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, '../public/images/upload'));
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname);
-    }
-})
-const upload = multer({ storage: storage });
-
+const { upload, cloudinary } = require('../middleware/multer-cloudinary');
 
 postController.post('/profile/create-post', upload.single('photo'), async (req, res) => {
     try {
@@ -24,35 +10,38 @@ postController.post('/profile/create-post', upload.single('photo'), async (req, 
         const userId = req.session.token;
         const auth = await User.findById(userId);
         const photo = req.file;
-        var newPost = new Post();
+        let newPost;
         if (!photo) {
             newPost = new Post({ userId: auth._id, username: auth.username, content });
             await newPost.save();
-            return res.status(201).json({ success: true })
+            return res.status(201).json({ success: true });
         }
-        const photoUrl = photo.path.split("public")[1];
-        newPost = new Post({ userId: auth._id, username: auth.username, content, photoUrl: photoUrl });
+        const photoUrl = photo.path;
+        newPost = new Post({ userId: auth._id, username: auth.username, content, photoUrl });
         await newPost.save();
-        return res.status(201).json({ success: true })
+
+        return res.status(201).json({ success: true, photoUrl });
     } catch (error) {
         return res.status(500).json({ message: error.message });
     }
-})
+});
 
 postController.post('/profile/delete-post', async (req, res) => {
     try {
         const { postId } = req.body;
         const post = await Post.findById(postId);
         if (post.photoUrl) {
-            const filePath = path.join(__dirname, '../public', post.photoUrl);
-            if (fs.existsSync(filePath)) {
-                await unlinkAsync(filePath);
-            }
+            // Template photo url: https://res.cloudinary.com/cloud_name/image/upload/v123456/uploads/photo.png
+            const parts = post.photoUrl.split('/');
+            const photoName = parts[parts.length - 1].split('.')[0];
+            const folderName = parts[parts.length - 2];
+            const cloudinaryPath = `${folderName}/${photoName}`;
+            await cloudinary.uploader.destroy(cloudinaryPath);
         }
         await Post.findByIdAndDelete(postId);
         return res.status(201).json({ success: true });
     } catch (error) {
-        return res.status(500).json({ message: error.message });
+        return console.log(error.message)
     }
 })
 
